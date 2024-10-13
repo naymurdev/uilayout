705f40bde6d2baba9eb3ef306fa5b0b125a68e08
naymurdev <naymurrahman178@gmail.com>
2024-10-13 12:09:29 +0600

    update horizontal-scroll

--- apps/web/src/registry/components/horizontal-scroll/horizontal-scroll.tsx
+++ apps/web/src/registry/components/horizontal-scroll/horizontal-scroll.tsx
@@ -6,12 +6,78 @@ import {
   useScroll,
   useTransform,
 } from 'framer-motion'
-//   import { throttle } from "throttle-debounce-ts";
 
-// import 'normalize.css'
 import { useEffect, useRef, useState } from 'react'
-import { throttle } from '@/lib/utils'
-import { items } from '@/components/common/constant'
+import { throttle } from '../../../lib/utils'
+
+export const items = [
+  {
+    id: 1,
+    url: 'https://images.unsplash.com/photo-1709949908058-a08659bfa922?q=80&w=1200&auto=format',
+  },
+  {
+    id: 2,
+    url: 'https://images.unsplash.com/photo-1548192746-dd526f154ed9?q=80&w=1200&auto=format',
+  },
+  {
+    id: 3,
+    url: 'https://images.unsplash.com/photo-1693581176773-a5f2362209e6?q=80&w=1200&auto=format',
+  },
+  {
+    id: 4,
+    url: 'https://images.unsplash.com/photo-1584043204475-8cc101d6c77a?q=80&w=1200&auto=format',
+  },
+  {
+    id: 5,
+    url: 'https://images.unsplash.com/photo-1709949908058-a08659bfa922?q=80&w=1200&auto=format',
+  },
+
+  {
+    id: 7,
+    url: 'https://images.unsplash.com/photo-1518599904199-0ca897819ddb?q=80&w=1200&auto=format',
+  },
+  {
+    id: 8,
+    url: 'https://images.unsplash.com/photo-1706049379414-437ec3a54e93?q=80&w=1200&auto=format',
+  },
+
+  {
+    id: 91,
+    url: 'https://images.unsplash.com/photo-1709949908219-fd9046282019?q=80&w=1200&auto=format',
+  },
+  {
+    id: 92,
+    url: 'https://images.unsplash.com/photo-1462989856370-729a9c1e2c91?q=80&w=1200&auto=format',
+  },
+  {
+    id: 10,
+    url: 'https://images.unsplash.com/photo-1475727946784-2890c8fdb9c8?q=80&w=1200&auto=format',
+  },
+  {
+    id: 11,
+    url: 'https://images.unsplash.com/photo-1546942113-a6c43b63104a?q=80&w=1200&auto=format',
+  },
+  {
+    id: 12,
+    url: 'https://images.unsplash.com/photo-1726551195764-f98a8e8a57c3?q=80&w=1200&auto=format',
+  },
+  {
+    id: 13,
+    url: 'https://images.unsplash.com/photo-1726551195795-612ca47c0b7d?q=80&w=1200&auto=format',
+  },
+  {
+    id: 14,
+    url: 'https://images.unsplash.com/photo-1532423622396-10a3f979251a?q=80&w=1200&auto=format',
+  },
+  {
+    id: 15,
+    url: 'https://images.unsplash.com/photo-1490750967868-88aa4486c946?q=80&w=1200&auto=format',
+  },
+  {
+    id: 16,
+    url: 'https://images.unsplash.com/photo-1496861083958-175bb1bd5702?q=80&w=1200&auto=format',
+  },
+]
 
 // * based on: https://gist.github.com/coleturner/34396fb826c12fbd88d6591173d178c2
 function useElementViewportPosition(ref: React.RefObject<HTMLElement>) {
@@ -83,7 +149,7 @@ export default function index() {
   }, [])
 
   return (
-    <div>
+    <>
       <section ref={mainRef}>
         <div className="w-full mx-auto" style={{ height: '300vh' }}>
           <div className="sticky top-0 h-screen w-full flex flex-col items-start justify-center overflow-hidden">
@@ -98,7 +164,7 @@ export default function index() {
                     key={item.id}
                     className="w-full flex-shrink-0 h-full object-cover"
                     src={item?.url}
-                    alt={item?.title}
+                    alt={'img'}
                   />
                 </motion.div>
               ))}
@@ -106,7 +172,7 @@ export default function index() {
           </div>
         </div>
       </section>
-      <footer className="group">
+      <footer className="group ">
         <h1 className="text-[16vw] group-hover:translate-y-4 translate-y-20 leading-[100%] uppercase font-semibold text-center bg-gradient-to-r from-gray-400 to-gray-800 bg-clip-text text-transparent transition-all ease-linear">
           ui-layout
         </h1>
@@ -114,6 +180,6 @@ export default function index() {
           Thanks for Scrolling
         </section>
       </footer>
-    </div>
+    </>
   )
 }
